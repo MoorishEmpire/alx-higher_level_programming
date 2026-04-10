d319704f487d78b1cf298c6d3c241b3e5a1179fd
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-10 12:52:45 +0100

    Floyd's cycle detection

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -16,7 +16,7 @@ int check_cycle(listint_t *list)
 
     slow = list;
     fast = list;
-    while (slow != NULL && fast != NULL)
+    while (fast != NULL && fast->next != NULL)
     {
         slow = slow->next;
         fast = fast->next->next;
