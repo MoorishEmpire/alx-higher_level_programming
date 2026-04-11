77581fb7c4e6b526215077552b503ba29d6996a2
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-11 16:50:13 +0100

    simple import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 add = __import__('add_0').add
-a = 1
-b = 2
-result = add(a, b)
-print(a, " + ", b, " = " + "{}".format(add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
