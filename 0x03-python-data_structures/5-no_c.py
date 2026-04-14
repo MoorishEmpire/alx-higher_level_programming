fa643f291234a1f639f18205772d7e3141cbc8ef
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-14 15:45:46 +0100

    Can you C me new?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if (len(my_string) == 0):
+        return None
     my_string = my_string.replace("c", "")
     my_string = my_string.replace("C", "")
     return (my_string)
