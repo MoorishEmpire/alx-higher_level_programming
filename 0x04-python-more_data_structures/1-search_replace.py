e9e5fbb539b3893ace31a91dd90a0404863e52d4
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-15 17:19:54 +0100

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    size = len(my_list)
+    if size == 0:
+        return []
+    new_list = [0] * size
+    for i in range(0, size):
+        if (my_list[i] == search):
+            new_list[i] = replace
+        else:
+            new_list[i] = my_list[i]
+    return new_list
