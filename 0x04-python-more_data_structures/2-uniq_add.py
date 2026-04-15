5fce0ff3ddd6b2aa11edb2f981780a0b00540166
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-15 21:03:08 +0100

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = []
-    for item in my_list:
-        if item not in new_list:
-            new_list.append(item)
-    return sum(new_list)
+    return sum(set(my_list))
+
