3530d59451c674b7d5e16604fbbab1c29839dda9
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-15 01:22:43 +0100

    linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,87 @@
+#include "lists.h"
+
+/**
+ * size_list - counts size of a list
+ * @head: the first of node of this list
+ * Return: size of the list
+ */
+
+int         size_list(listint_t *head)
+{
+    int size;
+
+    size = 0;
+    while (head)
+    {
+        size++;
+        head = head->next;
+    }
+    return size;
+}
+
+/**
+ * add_front - add a new node to the front of the list
+ * @head: the first node of this list
+ * @new: the new node to add to the front
+*/
+
+void        add_front(listint_t **head, listint_t *new)
+{
+    new->next = *head;
+    *head = new;
+}
+
+/**
+ * pop_front - pop a node from the from the front
+ * @head: the first node of this list
+ * Return: the node poped
+*/
+
+listint_t   *pop_front(listint_t **head)
+{
+    listint_t   *current;
+
+    current = *head;
+    *head = current->next;
+    return (current);
+}
+
+/**
+ * is_palindrome - check a singly linked list is a palindrome or not.
+ * @head: the first node of this list
+ * Return: 1 if is a palindrome, 0 if not
+*/
+
+int is_palindrome(listint_t **head)
+{
+    int         size;
+    int         i;
+    listint_t   *tmp;
+    listint_t   *beta;
+    listint_t   *new;
+
+    if (!*head || !(*head)->next)
+        return (1);
+
+    size = size_list(*head);
+    tmp = *head;
+    beta = NULL;
+
+    i = 0;
+    while (i < size / 2)
+    {
+        new = pop_front(&tmp);
+        add_front(&beta, new);
+        i++;
+    }
+    if (size_list(tmp) > size_list(beta))
+        tmp = tmp->next;
+    while (tmp && tmp->n == beta->n)
+    {
+        tmp = tmp->next;
+        beta = beta->next;
+    }
+    if (!tmp)
+        return (1);
+    return 0;
+}
