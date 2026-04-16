207c20c9f6be4f2718bd04e064f27f6c8c71172b
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-16 17:17:09 +0100

    Integers divsion with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
-        div = float(a / b)
-        return div
+        result = a / b
     except ZeroDivisionError:
-        return None
+        pass
     finally:
-        if (b == 0):
-            print("Inside result:", None)
-        else:
-            print("Inside results: ", div)
+        print("Inside result: {}".format(result))
+    return result
