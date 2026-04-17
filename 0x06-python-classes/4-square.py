c3ffc0da3e919aae46d40c0f046806f79572c1ed
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-17 19:45:29 +0100

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Initialize a new Square with a given size with a validation check."""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = int(size)
+
+    """ Getter for the private instance attribute size."""
+    @property
+    def size(self):
+        return self.__size
+
+    """ Setter for the private instance attribute size."""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """ Return the cuurent square area"""
+    def area(self):
+        return self.__size ** 2
