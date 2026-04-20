62cfef1a786f3911bfa91d48212b28027a088c2f
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-20 22:51:16 +0100

    Lazy matix multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -8,6 +8,9 @@ Module provide the matrices multiplication function.
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """
+    Function return a new_matrice the result of m_a and m_b multiplication.
+    """
     m_a = np.array(m_a)
     m_b = np.array(m_b)
 
