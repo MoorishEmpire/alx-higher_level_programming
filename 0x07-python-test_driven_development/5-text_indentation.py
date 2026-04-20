a462d6691f439c625b543c0490bd229e1586bb99
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-20 16:32:45 +0100

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Print a text with 2 new lines after each of these characters: ., ? and :
+
+This module provide the text_indentation function.
+"""
+def text_indentation(text):
+    """
+    Print the text with 2 new lines after line.
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    line = ""
+    for item in text:
+        if item not in [".", "?", ":"]:
+            line += item
+        else:
+            print(f"{line.strip()}{item}");
+            print()
+            line = ""
+    if len(line) > 0:
+        print(f"{line.strip()}", end="");
