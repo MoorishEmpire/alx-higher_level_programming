360b0249c027b33d21e522e86bb6b2bba2f12e0b
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-21 03:50:51 +0100

    Real defintion of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     """intializer for the new object created"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     """Geter for the private instance attribute width"""
     @property
