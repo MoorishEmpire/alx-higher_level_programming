8cc14e13acdde2f4034212eeb75d8c29ab505e81
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-23 07:22:14 +0100

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that contains Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
