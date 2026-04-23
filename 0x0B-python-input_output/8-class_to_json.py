81a319c3dc9acde385b955924926b6d8ed2b4b6a
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-23 19:50:29 +0100

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module contains the class class_to_json"""
+
+
 def class_to_json(obj):
+    """Returns the dictionary description with simple data stucture"""
     return obj.__dict__
