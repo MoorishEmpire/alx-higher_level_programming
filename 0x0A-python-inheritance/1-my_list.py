633767b61e28dac20dcbb22ed7ee8c038fcae1fd
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-23 03:24:26 +0100

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Module containes the Mylist class"""
+"""Module contains the Mylist class"""
 
 
 class MyList(list):
-    """Class inherets form list class prints a sorted list 
+    """Class inherets form list class prints a sorted list
     with using the print_sorted instance method.
     """
     def print_sorted(self):
+        """Prints the list sorted in ascending order."""
         print(sorted(self))
