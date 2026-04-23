9dfc681e61c539303278c3551116a7c659ed8d05
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-23 07:40:26 +0100

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,10 +8,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         super().__init__(size, size)
-        self.__size = size
-
+    
     def area(self):
         return super().area()
 
     def __str__(self):
-        return f"[Square] {self.__size}/{self.__size}"
+        return super().__str__()
