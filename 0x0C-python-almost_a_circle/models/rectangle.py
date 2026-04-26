c9536a1f89413d84763858c43f423b6ce110368d
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-26 08:25:21 +0100

    Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -65,3 +65,8 @@ class Rectangle(Base):
     def area(self):
         """Return the area of an object instance of Rectangle class"""
         return self.__width * self.__height
+
+    def display(self):
+        """Prints in stdout the Rectangle instance with the character #"""
+        for i in range(self.__height):
+            print("#" * self.__width)
