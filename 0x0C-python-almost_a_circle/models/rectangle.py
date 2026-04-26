fe618a387bba8a357c36c35fd6d2468df0ec42d8
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-26 08:22:18 +0100

    Area first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,4 +63,5 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """Return the area of an object instance of Rectangle class"""
         return self.__width * self.__height
