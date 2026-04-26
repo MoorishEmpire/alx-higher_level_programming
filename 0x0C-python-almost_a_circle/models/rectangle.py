2aad11b639d95a3ae2ed23a628e45ab193949e72
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-26 08:50:43 +0100

    Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that contains the Rectangle class"""
 from multiprocessing import Value
+from re import A
 from models.base import Base
 
 
@@ -80,3 +81,16 @@ class Rectangle(Base):
         """Returns a pritable format string of Rectangle instance"""
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
                 f"{self.__width}/{self.__height}")
+
+    def update(self, *args):
+        """Assigns an argument to each attribute"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
