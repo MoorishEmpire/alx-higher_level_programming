143664fd76e45ba3e6e5b64d396a23469bb57b8d
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-04-26 01:49:19 +0100

    Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,7 +7,7 @@ def pascal_triangle(n):
     Return a list of lists of integers representing the Pascal's triangle of n
     """
     if n <= 0:
-        return [[]]
+        return []
     new = [[1] * i for i in range(1, n + 1)]
 
     i = 0
