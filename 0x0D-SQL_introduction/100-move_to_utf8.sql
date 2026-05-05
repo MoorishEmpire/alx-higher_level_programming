eff68ce4bf9b7731622da6cff73d59d399043f0d
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-05-05 11:14:02 +0100

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,6 +2,11 @@
 -- of the database first_table in MySQL server.
 
 USE hbtn_0c_0;
+
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
