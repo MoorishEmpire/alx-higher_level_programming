98cf39d59ca03c8fdc5993d110ed785625328d12
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-05-05 09:13:12 +0100

    Count 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,6 @@
 -- Displays the number of records with id = 89 in the table "first_table" 
 -- of the database "hbtn_0c_0" in MySQL server.
 
-SELECT count(*) from first_table
-where first_table.id = 89;
+SELECT count(*) 
+from first_table
+where id = 89;
