296924e3d11ba86063977202e2130e2e1eaa51c7
ABDELKARIM ELMOSTAKIM <abdelkarimchampion@gmail.com>
2026-05-18 14:32:49 +0100

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Lists all states with a name starting with 'N'
-from the database 'hbtn_0e_0_usa'"
+from the database 'hbtn_0e_0_usa'
 """
 import MySQLdb
 import sys
@@ -16,8 +16,7 @@ if __name__ == "__main__":
         db=sys.argv[3]
         )
     cursor = mydb.cursor()
-    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%'\
-                    ORDER BY states.id")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
